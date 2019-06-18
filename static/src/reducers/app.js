b41ebdd6392912fd9e647f68863f445bdfdeb481
Olivier Yiptong <oyiptong@chromium.org>
2019-06-18 14:32:07 -0700

    simple persistence for debug mode

--- static/src/reducers/app.js
+++ static/src/reducers/app.js
@@ -3,7 +3,7 @@ import { UPDATE_PAGE, TOGGLE_DEBUG_MODE } from '../actions/app.js';
 const defaultState = {
   page: 'home',
   featuresEnabled: new Set(['nativefs']),
-  debug: false,
+  debug: window.localStorage.getItem('debug') ? true : false,
 };
 
 const app = (state = defaultState, action) => {
@@ -15,6 +15,11 @@ const app = (state = defaultState, action) => {
     };
   case TOGGLE_DEBUG_MODE:
     let debug = !state.debug;
+    if(debug) {
+      window.localStorage.setItem('debug', 1);
+    } else {
+      window.localStorage.removeItem('debug');
+    }
     return {
       ...state,
       debug,
