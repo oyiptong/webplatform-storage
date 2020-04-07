9f80964c0e6efa89b7fb6ad10fc28dc0fd75b02e
Olivier Yiptong <oyiptong@chromium.org>
2020-04-06 18:12:24 -0700

    return cached data

--- static/sw.js
+++ static/sw.js
@@ -1,16 +1,34 @@
+const CACHE_KEY = 'nativefs_1.0.0';
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-      caches.open('nativefs_ed').then((cache) => {
+      caches.open(CACHE_KEY).then(cache => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/main.js',
-          '/icon_192x192.png',
-          '/icon_512x512.png',
-          '/css/bootstrap.min.css',
-          '/webcomponents/webcomponents-loader.js',
+          './',
+          './index.html',
+          './main.js',
+          './manifest.json',
+          './icon_192x192.png',
+          './icon_512x512.png',
+          './css/bootstrap.min.css',
+          './webcomponents/webcomponents-loader.js',
         ]);
       }));
 });
 
-self.addEventListener('fetch', (event) => {});
+self.addEventListener('fetch', event => {
+  event.respondWith(async function() {
+    if (event.request.method != 'GET')
+      return fetch(event.request);
+
+    const cache = await caches.open(CACHE_KEY);
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+
+    return fetch(event.request);
+  }());
+});
